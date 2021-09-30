66ed01578344ea8a92ec047cc9a7fb7e601261ac
biddls <thomasbiddlecombe@gmail.com>
2021-09-30 13:38:48 +0100

    done testing as much as i can bear

--- contracts/lazyStreamer.sol
+++ contracts/lazyStreamer.sol
@@ -2,8 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-import {Istreamer} from "./interfaces/IpeepoPay.sol";
-
 // this contract allows for external users to keep track of who is going next
 // its way more gas costly but if its on an L2 then people dont care as much
 // and allows keepers to be way lazier when calling the code
@@ -11,12 +9,12 @@ import {Istreamer} from "./interfaces/IpeepoPay.sol";
 
 contract lazyStreamer {
 
-    address public streamerAddr;
+    address public peepoPay;
     address public admin;
 
     mapping(uint256 => Queue) dayQueue;
-    uint256 firstDay;
-    uint256 lastDay;
+    uint256 public firstDay;
+    uint256 public lastDay;
     /*
     keeps track of when it was last called so it can
     make sure that it doesn't call the draw down function
@@ -36,70 +34,58 @@ contract lazyStreamer {
         uint256 last;
     }
 
-    constructor() {
+    constructor(address _peepoPay) {
         admin = msg.sender;
         created = block.timestamp;
+        peepoPay = _peepoPay;
     }
 
     function pushStreams(uint256 _dayNo, uint256[] memory _ids) external {
         require(msg.sender == admin);
         require(_dayNo >= firstDay);
         for(uint256 i; i<_ids.length; i++){
-            // gets the queue for the day // gets the next spot on the queue then fills that spot int
-            dayQueue[_dayNo].IDs[dayQueue[_dayNo].last] = _ids[i];
-            // gets the same queue as above and increments the pointer to the next position
-            dayQueue[_dayNo].IDs[dayQueue[_dayNo].last]++;
+            _pushStream(_dayNo, _ids[i]);
         }
+    }
+
+    function pushStream(uint256 _dayNo, uint256 _id) public {
+        require(msg.sender == admin);
+        require(_dayNo >= firstDay);
+        _pushStream(_dayNo, _id);
+    }
+
+    function _pushStream(uint256 _dayNo, uint256 _id) internal {
+        // assigns the ID of the stream in the next available slot
+        dayQueue[_dayNo].IDs[dayQueue[_dayNo].last] = _id;
+        // updates the position of the pointer
+        dayQueue[_dayNo].last++;
+        // updates the bounds for the day queue
+        lastDay = _dayNo >= lastDay ? _dayNo++ : lastDay;
     }
 
     // may be able to run out of gas and crash the code
     function pop() external returns (uint256 ID) {
-        require(msg.sender == admin);
-        // if the next day is empty try the next day
-        if(dayQueue[firstDay].first == dayQueue[firstDay].last) {
-            // make sure that there is a day
-            require(firstDay < lastDay);
+        require(msg.sender == admin || msg.sender == address(this), "not admin");
+        // if the day has data call it
+        if (dayQueue[firstDay].first < dayQueue[firstDay].last && firstDay <= lastDay) { // if there is a day
+            // gets the indexes for the queue of that day
+            uint256 _first = dayQueue[firstDay].first;
+            uint256 _last = dayQueue[firstDay].last;
+            // gets the ID number
+            uint256 _id = dayQueue[firstDay].IDs[_first];
+            // clear data
+            delete dayQueue[firstDay].IDs[_first];
+            // update pointers
+            dayQueue[firstDay].first++;
+            return _id;
+        } else {
             //clear data
             delete dayQueue[firstDay];
             // tells it to move onto the next day
             firstDay++;
             // calls the same function again searching for the next day to use
-            return pop();
-        } else { // if there is a day
-            // gets the indexes for the queue of that day
-            uint256 _first = dayQueue[firstDay].first;
-            uint256 _last = dayQueue[firstDay].last;
-            // if there is data in the day
-            if(_first < _last){
-                // update pointers
-                dayQueue[firstDay].first++;
-                // gets the ID number
-                uint256 _id = dayQueue[firstDay].IDs[_first];
-                // clear data
-                delete dayQueue[firstDay].IDs[_first];
-                return _id;
-            } else {
-                // there is no data left in that day number
-                if(firstDay < lastDay){
-                    // deletes the day
-                    delete dayQueue[firstDay];
-                    // updates pointer
-                    firstDay++;
-                    // calls function again to try again
-                    pop();
-                } else {
-                    // there are no more streams to draw down from so that's the end of it
-                    revert();
-                }
-            }
+            return firstDay < lastDay ? this.pop() : 2^256-1;
         }
-
-        // not sure on this kinda sus
-        // it should move up the sinceLast number over time as days are cleared out
-        if((firstDay * 86400) + created > sinceLast){
-            sinceLast = block.timestamp;
-        }
-        return 1;
     }
 
     function removeStream(uint256 _dayNo, uint256 _IDIndex) external {
